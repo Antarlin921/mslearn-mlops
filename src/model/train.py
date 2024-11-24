ee1a742eda779b20185b850485419826f940fc4c
Antarlin Chanda <antarlin921@gmail.com>
2024-11-24 12:18:50 +0530

    Updating the YML

--- src/model/train.py
+++ src/model/train.py
@@ -3,15 +3,18 @@
 import argparse
 import glob
 import os
+import mlflow
 
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
+    mlflow.autolog()
 
 
     # read data
@@ -19,9 +22,11 @@ def main(args):
 
     # split data
     X_train, X_test, y_train, y_test = split_data(df)
+    
+    with mlflow.start_run():
 
-    # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+        # train model
+        train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
 def get_csvs_df(path):
@@ -34,6 +39,12 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test=train_test_split(X, y, test_size=0.30, random_state=0)
+    
+    return  X_train, X_test, y_train, y_test 
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
